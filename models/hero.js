4d3d35445134c894ff307df5a3c59a235519f381
Daniel <dannyking07@outlook.es>
2023-08-08 08:24:46 -0500

    feat [models -> hero]

--- models/hero.js
+++ models/hero.js
@@ -2,40 +2,29 @@ const mongoose = require("mongoose")
 const Schema = mongoose.Schema;
 
 const HeroSchema = new Schema({
-  _id: {
+
+  heroId:{
     type: Number,
     required: true,
-    unique: true,
-    default: 1
+    unique : true
   },
-
-  hero: {
+  namehero: {
     type: String,
+    required : true,
+    maxLength : 30
+  },
+  imgHero: {
+    type: String,
+    required : false
+  },
+  done: {
+    type: Boolean,
     required: true,
-    maxLength: 50
+    default:false
   }
-
-
-
 
 });
 
-// Hook pre-save para generar el ID personalizado antes de guardar el documento
-HeroSchema.pre('save', async function (next) {
-  if (!this.isNew) {
-    // Si el documento ya existe y está siendo actualizado, no generamos un nuevo ID
-    return next();
-  }
+const Hero = mongoose.model('Hero', HeroSchema)
 
-  try {
-    const lastRecord = await this.constructor.findOne().sort({ _id: -1 });
-    const newId = lastRecord ? lastRecord._id + 1 : 1;
-    this._id = newId;
-    next();
-  } catch (error) {
-    next(error);
-  }
-});
-
-
-module.exports = mongoose.model("hero", HeroSchema)
+module.exports = Hero
