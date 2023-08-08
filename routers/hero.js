6d617c0be8403cf7f536bf9ffcd1578176ef0d8b
Daniel <dannyking07@outlook.es>
2023-08-08 08:24:02 -0500

    feat [routers -> hero]

--- routers/hero.js
+++ routers/hero.js
@@ -1,11 +1,11 @@
-const express = require('express')
+const express = require(`express`)
 const router = express.Router()
 const heroControllers = require('../controllers/hero')
 
 router.get('/', heroControllers.get)
-router.get('/:id', heroControllers.getById)
+router.get('/:heroId', heroControllers.getById)
 router.post('/create', heroControllers.create)
-router.patch('/update/:id', heroControllers.update)
-router.delete('/delete/:id', heroControllers.delete)
+router.patch('/update/:heroId', heroControllers.update)
+router.delete('/delete/:heroId', heroControllers.delete)
 
 module.exports = router
